2b1fb0b472873bbad7786e4e1212cc729b25026d
Martin Rekalde Hernando <marekald@c3r4s5.42urduliz.com>
2026-01-18 11:26:59 +0100

    planteamiento de lo necesario

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: marekald <marekald@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 17:03:41 by marekald          #+#    #+#             */
-/*   Updated: 2026/01/17 20:52:11 by marekald         ###   ########.fr       */
+/*   Updated: 2026/01/18 11:26:04 by marekald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,13 @@ int	main(int argc, char **argv)
 	(void)argv;
 	ft_check_args(argv);
 	stack_a = (t_stack **)malloc(sizeof(t_stack));
-	if (!stack_a)
-		return (NULL);
 	*stack_a = NULL;
-	
+	//llenar el stack a.
+	//si está ordenado no devolver nada y liberar memoria.
+	stack_b = (t_stack **)malloc(sizeof(t_stack));
+	stack_b = NULL;
+	//ordenar stack e imprimir los movimientos.
+	free_stack(stack_a);
+	free_stack(stack_b);
+	return (0);
 }
